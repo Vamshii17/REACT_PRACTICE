0065b61c37d381d04ffdcbf76b2ee67472800bcc
unknown <telukuntlavamshikrishna@gmail.com>
2024-06-03 11:41:17 +0530

    Class Component and Styling

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "./style.css"
 import HeadingComponent from "./Components/Class_Component/heading_component";
 import AccordionComponent from "./Components/Accordion/accordion";
 import TableComponent from "./Components/Table/tableBootStrap";
-import navBarComponent from "./Components/Nav_Bar/navBar";
+import NavBarComponent from "./Components/Nav_Bar/navBar";
+import CarouselComponent from "./Components/Carousels/Carousels";
+import ProgressBarComponent from "./Components/Progress_Bars/ProgressBars";
+import SpinnerComponent from "./Components/Spinners/SpinnersComponent";
+import ToastComponent from "./Components/Toasts/Toasts_Component";
+import ModalComponent from "./Components/Modals/Modals_Component";
 function Hello(){
   return(
     <>
-    <navBarComponent></navBarComponent>
+    <NavBarComponent></NavBarComponent>
+    <br></br>
+    <br></br>
+    <CarouselComponent></CarouselComponent>
     <br></br>
     <br></br>
     <AccordionComponent></AccordionComponent>
@@ -16,8 +24,16 @@ function Hello(){
     <TableComponent></TableComponent>
     <br></br>
     <br></br>
+    <ProgressBarComponent></ProgressBarComponent>
     <br></br>
     <br></br>
+    <SpinnerComponent></SpinnerComponent>
+    <br></br>
+    <br></br>
+    <ToastComponent></ToastComponent>
+    <br></br>
+    <br></br>
+    <ModalComponent></ModalComponent>
     <br></br>
     <br></br>
     <HeadingComponent></HeadingComponent>
--- src/Components/Nav_Bar/navBar.js
+++ src/Components/Nav_Bar/navBar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-const navBarComponent=()=>{
+const NavBarComponent=()=>{
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -31,4 +31,4 @@ const navBarComponent=()=>{
   );
 }
 
-export default navBarComponent;
+export default NavBarComponent;
